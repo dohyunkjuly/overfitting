915d8a831c2ac2b9a0c2210b2a1820e7f3f30a6b
dohyunkjuly <157889441+dohyunkjuly@users.noreply.github.com>
2024-08-03 13:50:55 +0900

    Initialized Broker class for handling executions

---
+++ ccbt/broker.py
@@ -0,0 +1,46 @@
+import abc
+from typing import List, Dict
+from ccbt.order import Order
+from ccbt.position import Position
+
+class Broker:
+    def __init__(self,
+                 data,
+                 fx_slippage=0,
+                 linear_slippage=0,
+                 fx_commission=0,
+                 linear_commission=0):
+        
+        self.data = data
+        self.fx_slippage = fx_slippage
+        self.linear_slippage = linear_slippage
+        self.fx_commission = fx_commission
+        self.linear_commission = linear_commission
+
+        self.open_orders: List[Order] = []
+        self.new_orders: List[Order] = []
+        self.orders: Dict[Order] = {}
+        self.position: Dict[Position] = {}
+
+    def __repr__(self):
+        return """
+{class_name}(
+    data={data},
+    fx_slippage={fx_slippage},
+    linear_slippage={linear_slippage},
+    fx_commission={fx_commission},
+    linear_commission={linear_commission},
+    open_orders={open_orders},
+    new_orders={new_orders},
+    orders={orders},
+    position={position})
+""".strip().format(class_name=self.__class__.__name__,
+                   data=self.data,
+                   fx_slippage=self.fx_slippage,
+                   linear_slippage=self.linear_slippage,
+                   fx_commission=self.fx_commission,
+                   linear_commission=self.linear_commission,
+                   open_orders=self.open_orders,
+                   new_orders=self.new_orders,
+                   orders=self.orders,
+                   position=self.position)
