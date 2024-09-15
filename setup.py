b2787366cda59e78587779d4a3c4b3321826a425
dohyunkjuly <157889441+dohyunkjuly@users.noreply.github.com>
2024-09-15 16:38:02 +0900

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Overfitting',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A Robust CryptoCurrency Backtesting Library.',
+    author='Dohyun',
+    install_requires=[
+    ],
+)
