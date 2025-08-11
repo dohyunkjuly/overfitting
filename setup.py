35aadbebebf3e31f951a41056a7b78106835fb84
Dohyun <157889441+dohyunkjuly@users.noreply.github.com>
2025-08-11 22:57:52 +0900

    version 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='overfitting',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     license='MIT',
     description='A Robust Futures CryptoCurrency Backtesting Library.',
